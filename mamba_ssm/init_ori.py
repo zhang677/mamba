4c24b3af95d13a03635008c38445eaf40bcda033
zhang677 <zgh23@stanford.edu>
2025-02-23 01:44:43 +0000

    Expose more blocks

--- mamba_ssm/init_ori.py
+++ mamba_ssm/init_ori.py
@@ -3,6 +3,4 @@ __version__ = "2.2.4"
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from mamba_ssm.modules.mamba_simple import Mamba
 from mamba_ssm.modules.mamba2 import Mamba2
-from mamba_ssm.modules.mamba2_simple import Mamba2Simple
-from mamba_ssm.modules.block import Block
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
